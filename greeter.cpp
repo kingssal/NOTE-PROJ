74c7fc9865445820b545c1226db83f09c7b0c52f
younghun02 <152832334+younghun02@users.noreply.github.com>
2023-12-10 11:14:23 +0900

    중복 헤더 파일 제거, printTiltle, printOption 메소드로 수정 - 오영훈

--- greeter.cpp
+++ greeter.cpp
@@ -1,98 +1,98 @@
 #include "greeter.h"
-#include <iostream>
-#include <algorithm>
-#include <cctype>
-#include <cstdlib>
-#include <ctime> 
+//#include <iostream>
+//#include <algorithm>
+//#include <cctype>
+//#include <cstdlib>
+//#include <ctime> 
 
-Greeter::Greeter (NoteDB n, WordDB w, RoadMap* r) : noteDB(n), wordDB(w), rootRoadMap(r) {}
-void printTitle(){
+Greeter::Greeter(NoteDB n, WordDB w, RoadMap* r) : noteDB(n), wordDB(w), rootRoadMap(r) {}
+void Greeter:: printTitle() {
     cout << "               :::        ::::::::::: ::::    :::  ::::::::  ::::::::::: :::::::::: \n";
-	cout << "               :+:            :+:     :+:+:   :+: :+:    :+:     :+:     :+:        \n";
-	cout << "               +:+            +:+     :+:+:+  +:+ +:+    +:+     +:+     +:+        \n";
-	cout << "               +#+            +#+     +#+ +:+ +#+ +#+    +:+     +#+     +#++:++#   \n";
-	cout << "               +#+            +#+     +#+  +#+#+# +#+    +#+     +#+     +#+        \n";
-	cout << "               #+#            #+#     #+#   #+#+# #+#    #+#     #+#     #+#        \n";
-	cout << "               ########## ########### ###    ####  ########      ###     ########## \n";;
+    cout << "               :+:            :+:     :+:+:   :+: :+:    :+:     :+:     :+:        \n";
+    cout << "               +:+            +:+     :+:+:+  +:+ +:+    +:+     +:+     +:+        \n";
+    cout << "               +#+            +#+     +#+ +:+ +#+ +#+    +:+     +#+     +#++:++#   \n";
+    cout << "               +#+            +#+     +#+  +#+#+# +#+    +#+     +#+     +#+        \n";
+    cout << "               #+#            #+#     #+#   #+#+# #+#    #+#     #+#     #+#        \n";
+    cout << "               ########## ########### ###    ####  ########      ###     ########## \n";;
     cout << endl;
 };
-void Greeter::printRoadMap(){
+void Greeter::printRoadMap() {
     rootRoadMap->showBranch();
 }
-void printOption(){
-	cout << "        --------------------------------------------------------------------------\n";
-	cout << "             1.New note                                5.Open roadmap\n";
-	cout << "             2.Open note                               6.Move parent roadmap\n";
-	cout << "             3.Delete note                             7.Delete raodmap\n";
-	cout << "             4.New roadmap                             8.Word Finder\n";
-	cout << "        --------------------------------------------------------------------------\n";
-	cout << endl;
-	cout << "        Enter Number:";
+void Greeter:: printOption() {
+    cout << "        --------------------------------------------------------------------------\n";
+    cout << "             1.New note                                5.Open roadmap\n";
+    cout << "             2.Open note                               6.Move parent roadmap\n";
+    cout << "             3.Delete note                             7.Delete raodmap\n";
+    cout << "             4.New roadmap                             8.Word Finder\n";
+    cout << "        --------------------------------------------------------------------------\n";
+    cout << endl;
+    cout << "        Enter Number:";
 }
 void Greeter::selectOption() {
 
-	string selectedOption;
-	cin >> selectedOption;
-	int select;
+    string selectedOption;
+    cin >> selectedOption;
+    int select;
 
-	if (all_of(selectedOption.begin(), selectedOption.end(), [](unsigned char c) { return std::isdigit(c); })) {
-		select = stoi(selectedOption);
-	}
-	else {
-		select = -1;
-	}
+    if (all_of(selectedOption.begin(), selectedOption.end(), [](unsigned char c) { return std::isdigit(c); })) {
+        select = stoi(selectedOption);
+    }
+    else {
+        select = -1;
+    }
 
-	#ifdef _WIN32
+#ifdef _WIN32
     // Windows
-        system("cls");
-    #else
+    system("cls");
+#else
     // Unix (Linux, macOS, etc.)
-        system("clear");
-    #endif 
+    system("clear");
+#endif 
 
-	switch (select) {
-        case 1: {
-            greeterAddNote();
-            break;
-        }
-        case 2: {
-            greeterOpenNote();
-            break;
-        }
-        case 3: {
-            greeterDeleteNote();
-            break;
+    switch (select) {
+    case 1: {
+        greeterAddNote();
+        break;
+    }
+    case 2: {
+        greeterOpenNote();
+        break;
+    }
+    case 3: {
+        greeterDeleteNote();
+        break;
 
-        }
-        case 4: {
-            greeterAddRoadMap();
-            break;
-        }
-        case 5: {
-            greeterOpneRoadMap(); 
-            break;
-        }
-        case 6: {
-            greeterCloseRoadMap();
-            break;
-        }
-        case 7: {
-            greeterDeleteRoadMap();
-            break;
-        }
-        case 8: {
-            greeterSearchWord();
-            break;
-        }
-        default: 
-        {
-            cout << "Entered it incorrectly. Enter a number between 1 and 8.\n";
-            system("pause");
-	    }
+    }
+    case 4: {
+        greeterAddRoadMap();
+        break;
+    }
+    case 5: {
+        greeterOpneRoadMap();
+        break;
+    }
+    case 6: {
+        greeterCloseRoadMap();
+        break;
+    }
+    case 7: {
+        greeterDeleteRoadMap();
+        break;
+    }
+    case 8: {
+        greeterSearchWord();
+        break;
+    }
+    default:
+    {
+        cout << "Entered it incorrectly. Enter a number between 1 and 8.\n";
+        system("pause");
+    }
 
-	}
+    }
 }
-void Greeter::greeterAddNote(){
+void Greeter::greeterAddNote() {
     srand((unsigned int)time(NULL));
     int noteID;
     cout << "        Enter a note ID(If you enter an invalid value, set the ID randomly):";
@@ -112,10 +112,10 @@ void Greeter::greeterAddNote(){
     noteDB.addNote(noteID, noteName, "");
     rootRoadMap->addBranch(noteName);
 } //1
-void Greeter::greeterOpenNote(){
+void Greeter::greeterOpenNote() {
     int noteID;
     cout << "        Enter a Note ID:";
-    if(cin >> noteID && noteDB.search(noteID).getNoteID() != -1){
+    if (cin >> noteID && noteDB.search(noteID).getNoteID() != -1) {
         noteDB.search(noteID).printNote();
     }
     else {
@@ -125,10 +125,10 @@ void Greeter::greeterOpenNote(){
     }
     rootRoadMap->addBranch(noteDB.search(noteID).getTitle());
 } //2
-void Greeter::greeterDeleteNote(){
+void Greeter::greeterDeleteNote() {
     int noteID;
     cout << "        Enter a Note ID:";
-    if(cin >> noteID && noteDB.search(noteID).getNoteID() != -1){
+    if (cin >> noteID && noteDB.search(noteID).getNoteID() != -1) {
         noteDB.deleteNote(noteID);
     }
     else {
@@ -138,7 +138,7 @@ void Greeter::greeterDeleteNote(){
     }
     rootRoadMap->deleteBranch(noteDB.search(noteID).getTitle());
 } //3
-void Greeter::greeterAddRoadMap(){
+void Greeter::greeterAddRoadMap() {
     string roadMapName;
     cout << "        Enter a roadmap name: ";
     getline(cin, roadMapName);
@@ -146,24 +146,24 @@ void Greeter::greeterAddRoadMap(){
     rootRoadMap->addBranch(roadMapName);
     rootRoadMap->addChildRoadMap(RoadMap);
 } //4
-void Greeter::greeterOpneRoadMap(){
+void Greeter::greeterOpneRoadMap() {
     string roadMapName;
     cout << "        Enter a roadmap name: ";
     getline(cin, roadMapName);
     rootRoadMap = &rootRoadMap->findRoadMap(roadMapName);
 } //5
-void Greeter::greeterCloseRoadMap(){
+void Greeter::greeterCloseRoadMap() {
     string roadMapName;
     rootRoadMap = rootRoadMap->getParentRoadMap();
 } //5
-void Greeter::greeterDeleteRoadMap(){
+void Greeter::greeterDeleteRoadMap() {
     string roadMapName;
     cout << "        Enter a roadmap name: ";
     getline(cin, roadMapName);
     rootRoadMap->deleteBranch(roadMapName);
     rootRoadMap->deleteChildRoadMap(roadMapName);
 } //6
-void Greeter::greeterSearchWord(){
+void Greeter::greeterSearchWord() {
     string word;
     cout << "        Enter a roadmap word: ";
     getline(cin, word);
