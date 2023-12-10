7747b6edd52a5fa81ef0668121b33ca60adfec1c
younghun02 <152832334+younghun02@users.noreply.github.com>
2023-12-10 11:11:56 +0900

    main.cpp 중복 호출된 헤더 파일  없앰 - 오영훈

--- main.cpp
+++ main.cpp
@@ -1,31 +1,31 @@
-#include "note.h"
-#include "noteDB.h"
-#include "word.h"
-#include "wordDB.h"
+//#include "note.h"
+//#include "noteDB.h"
+//#include "word.h"
+//#include "wordDB.h"
 #include "greeter.h"
 
 
 using namespace std;
- 
-int main(int argc, char const *argv[])
+
+int main(int argc, char const* argv[])
 {
     NoteDB noteDB;
     WordDB wordDB;
     RoadMap rootRoadmap("rootRoadmap");
     Greeter greeter(noteDB, wordDB, &rootRoadmap);
 
-	while (1) {
-		#ifdef _WIN32
+    while (1) {
+#ifdef _WIN32
         // Windows
-            system("cls");
-        #else
+        system("cls");
+#else
         // Unix (Linux, macOS, etc.)
-            system("clear");
-        #endif
+        system("clear");
+#endif
         greeter.printTitle();
         greeter.printRoadMap();
-		greeter.printOption();
-		greeter.selectOption();
-	}
+        greeter.printOption();
+        greeter.selectOption();
+    }
     return 0;
 }
